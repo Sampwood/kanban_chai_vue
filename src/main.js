863937284a3cfb57cfdc9738971fa50e9a26e2e7
Sampwood <sampwood@163.com>
2019-01-14 17:29:11 +0800

    fix: docs文档用来生成静态网页

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Icon from 'vue-awesome/components/Icon'
-import WildVue from 'wildvue'
 import App from './App'
 import router from './router'
 import store from './vuex/store'
@@ -23,7 +22,6 @@ if (process.env.NODE_ENV === 'production') {
 Vue.config.productionTip = false
 
 Vue.component('icon', Icon)
-Vue.use(WildVue)
 
 Vue.directive('focus', {
   inserted: function (el) {
