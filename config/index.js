6cbfc426f0fa62abca33a55bc6ceac0c6b170d77
Sampwood <sampwood@163.com>
2019-01-16 17:57:50 +0800

    fix: 开发环境proxyTable调整

--- config/index.js
+++ config/index.js
@@ -34,7 +34,7 @@ module.exports = {
     proxyTable: {
       '/api': {
         target: 'https://www.easy-mock.com/mock/5c2d79a8873e4126e1daa1fd/api', // 接口的域名
-        // secure: false,  // 如果是https接口，需要配置这个参数
+        secure: false,  // 如果是https接口，需要配置这个参数
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
         pathRewrite: {
           '^/api': ''
